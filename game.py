85d64c195a0328893f339176793a9e580e5f2675
TamaraAlhajj <tamara.alhajj@carleton.ca>
2019-02-23 09:15:41 -0500

    All searches complete: bfs, dfs, a*. Must fix hueristics

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
 from collections import deque
 from random import randint
+from time import clock
+from queue import PriorityQueue
+from math import sqrt
 
-# TODO refactor!!
+# TODO refactor!! Print time elapsed
 
 ############################ Initial Setup ############################
 
@@ -9,9 +12,6 @@ from random import randint
 
 # Dimensions #
 board = 16
-
-# Defining all ant moves, NWSE -- logic is wonky here TODO #
-vectors = [[0, 1], [-1, 0], [0, -1], [1, 0]]
 
 """ 
 # Defining all spider movement partterns for playable game #
@@ -40,14 +40,16 @@ moves = [
     [0, -1]
 ]
 
-direction = vectors[0]
-
 # State space as a set (hash table) O(1) membership check #
 seen_states = {}
 
 # Search path #
 path = deque()
 
+# Defining all ant moves #
+vectors = [[0, 1], [-1, 0], [0, -1], [1, 0]]
+
+direction = vectors[0]
 #######################################################################
 
 ###### ant stuff ######
@@ -72,13 +74,13 @@ def ant_init():
 
 def ant_dir(init):
     if(init[0] == 0):
-        return vectors[2]      
-    elif(init[0] == 16):
-        return vectors[0]
-    elif(init[1] == 0):
+        return vectors[3]      
+    elif(init[0] == 15):
         return vectors[1]
+    elif(init[1] == 0):
+        return vectors[0]
     else:
-        return vectors[3]
+        return vectors[2]
 
 #############################################################################
 
@@ -91,28 +93,31 @@ def off_board(state):
 
 ### UPDATE CREATURES ###
 def make_move(state, move):
-    if(off_board(state)):
-        return False
     spider = state[:2]
     ant = state[2:]
     new_spider = [spider[0]+move[0], spider[1]+move[1]]
     new_ant = [ant[0]+direction[0], ant[1]+direction[1]]
     new_state = new_spider + new_ant
+    if(off_board(new_state)):
+        return False
     return new_state
 
 
 ### GENERATE POSSIBLE MOVES ###
 def expand(state):
+    global seen_states, path, fringe
+
     for move in moves:
         new_state = make_move(state, move)
         if((new_state != False) and (tuple(new_state) not in seen_states)):
             path.append(new_state)
             seen_states[tuple(new_state)] = state
 
+
 ### PRINT CURRENT BOARD ###
 def display_board(state):
 
-    print(state)
+    print('\n' + '-'*27 + 'spider: {}   ant: {}'.format(state[:2], state[2:]) + '-'*27)
 
     # spider location
     sx = state[0]
@@ -140,66 +145,173 @@ def display_board(state):
         print()
 
 ### ONCE FOUND SHOW PATH TO GOAL ###
-def display_path(goal):
+def display_path(goal, blind=True):
     current = goal
-    while(current != 0):
+    moves = 0
+    while(current != None):
+        moves += 1
         display_board(current)
         current = seen_states[tuple(current)]
+    print('\nDONE! {} states searched, {} move solution found. \nScroll up to see solution.'.format(len(seen_states), moves))
 
 
 ### CHECK IF GOAL STATE REACHED ###
 def goal_check(state):
     if(state[0] == state[2] and state[1] == state[3]):
-        display_path(state)
-        return False
-    return True
+        return True
+    return False
 
+def reset():
+    global seen_states, path
+
+    seen_states = {}
+    path = deque()
+
+    return
 
 def bfs(init):
-    # false once goal state game over is reached #
+    # searching is false once goal state game over is reached
     state = init
-    searching = True
 
-    while (searching):
-        if(path):
-            state = path.popleft()
-            expand(state)
-            searching = goal_check(state)
-        else:
-            searching = False
-            print('---end of game---')
+    while (path):
+        state = path.popleft()
+        expand(state)
+        if(goal_check(state)):
+            display_path(state)
+            break
 
 
 def dfs(init):
     # false once goal state game over is reached #
     state = init
-    searching = True
 
-    while (searching):
-        if(path):
-            state = path.pop()
-            expand(state)
-            searching = goal_check(state)
-        else:
-            searching = False
-            print('---end of game---')
+    while (path):
+        state = path.pop()
+        expand(state)
+        if(goal_check(state)):
+            display_path(state)
+            break
+
+def diagonal(state):
+    # Diagonal distance on a square grid
+    sx = state[0]
+    sy = state[1]
+    ax = state[2]
+    ay = state[3]
+
+    D = 1 # min cost to move
+    D2 = 4 # cost of knight move (diagonal)
+
+    dx = abs(sx - ax)
+    dy = abs(sy - ay)
+    return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
 
 
-def A_star():
-    pass
+def euclid(state):
+    # pythagorean distance on a square grid
+    sx = state[0]
+    sy = state[1]
+    ax = state[2]
+    ay = state[3]
+
+    dx = abs(sx - ax)
+    dy = abs(sy - ay)
+    return sqrt(dx**2 + dy**2)
+
+def avg(state):
+    c1 = diagonal(state)
+    c2 = euclid(state)
+    return (c1 + c2)/2
+
+def A_star(init, heuristic):
+    global moves, fringe
+
+    if(heuristic == 1):
+        heuristic = diagonal
+    elif(heuristic == 2):
+        heuristic = euclid
+    else:
+        heuristic = avg
+
+    # cost_so_far , Dijkstra dist value
+    # came_from , Dijkstra dist value
+    # fringe , ordered by cost to get to a state + heuristic
+
+    fringe = PriorityQueue()
+    came_from = {}
+    cost_so_far = {}
+
+    fringe.put(tuple(init), 0)
+    came_from[tuple(init)] = None
+    cost_so_far[tuple(init)] = 0
+    move_count = 0
+
+    while(not fringe.empty()):
+        current = fringe.get()
+
+        if(goal_check(current)):
+            count = 0
+            while(current != None):
+                count += 1
+                display_board(current)
+                current = came_from[current]
+            print('\nDONE! {} states generated, {} move solution found. \nScroll up to see solution.'.format(move_count, count))
+            break
+        
+        for move in moves:
+            new_move = make_move(current, move)
+            if((new_move != False)):
+                new_move = tuple(new_move)
+                new_cost = cost_so_far[current] + 1 
+                if(new_move not in cost_so_far or new_cost < cost_so_far[new_move]):
+                    cost_so_far[new_move] = new_cost
+                    priority = new_cost + heuristic(list(new_move))
+                    fringe.put(new_move, priority)
+                    move_count += 1
+                    came_from[new_move] = current
 
 
 def play():
-    spider = [randint(0,15), randint(0,15)]
-    ant = ant_init()
-    initial_state = spider + ant
-    direction = ant_dir(initial_state)
+    global direction, seen_states, path
 
-    seen_states[tuple(initial_state)] = 0
-    path.append(initial_state)
+    playing = True
 
-    #bfs(initial_state)
-    dfs(initial_state)
+    while(playing):
+
+        spider = [randint(0,15), randint(0,15)]
+        ant = ant_init()
+        initial_state = spider + ant
+        
+        # globals
+        direction = ant_dir(ant)
+        seen_states[tuple(initial_state)] = None
+        path.append(initial_state)
+
+        search = input('Choose an AI! (bfs, dfs, a*) : ')
+
+        if(search == 'bfs'):
+            start = clock()
+            bfs(initial_state)
+            elapsed = clock()
+            print('Run Time: {:0.3f} seconds\n'.format(elapsed - start))
+        elif(search == 'dfs'):
+            start = clock()
+            dfs(initial_state)
+            elapsed = clock()
+            print('Run Time: {:0.3f} seconds\n'.format(elapsed - start))
+        elif(search == 'a*'):
+            start = clock()
+            h = input("Which heuristic? (1 diagonal, 2 euclid, 3 avg) : ")
+            A_star(initial_state, h)
+            elapsed = clock()
+            print('Run Time: {:0.3f} seconds\n'.format(elapsed - start))
+        else:
+            print('Invalid search')
+        reset()
+        check = input('Try again? (y/n) : ')
+        if(check == 'n'):
+            playing = False
+
 
 def main():
     play()
